e6c7c7dbb2dc438dd3c016120b739b182e07128b
DanCai <113663226+dan61013@users.noreply.github.com>
2025-09-04 17:15:40 +0800

    1724-CUSTOMER-WHO-VISITED-BUT-DID-NOT-MAKE-ANY-TRANSACTIONS Stats: Time: 439 ms (18.26%), Space: 69 MB (5.46%) - LeetHub

---
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def find_customers(visits: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataFrame:
+    # merge two tables
+    merge_df = visits.merge(
+        transactions,
+        how='left',
+        on='visit_id'
+    )
+    # filtering: isna
+    merge_df = merge_df[merge_df['transaction_id'].isna()]
+    # groupby each customerID and count NULL
+    cnt_ser = merge_df.groupby(
+        by=['customer_id'],
+        dropna=False
+    ).agg(count_no_trans=('transaction_id', lambda x: x.isna().sum()))
+
+    return cnt_ser.reset_index()
