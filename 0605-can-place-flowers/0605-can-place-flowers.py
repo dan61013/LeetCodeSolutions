9ecdce96d09f02f16b69497a56bd98fd901c86a5
DanCai <113663226+dan61013@users.noreply.github.com>
2025-09-08 17:34:32 +0800

    0605-CAN-PLACE-FLOWERS Stats: Time: 14 ms (15.98%), Space: 17.9 MB (96.64%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,16 +1,21 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        # init the previous pointer equal 0
         prev_ptr = 0
+        # interate
         for i in range(len(flowerbed)):
+            # break the loop (n == 0)
             if not n:
                 return True
-            # Set prev & next pointer
+            # set prev & next pointer
             if i:
                 prev_ptr = flowerbed[i-1]
             if i != len(flowerbed) - 1:
                 next_ptr = flowerbed[i+1]
             else:
+                # for the last element
                 next_ptr = 0
+            # plant new flower when continuously 3 position are available
             if not prev_ptr and not flowerbed[i] and not next_ptr:
                 flowerbed[i] = 1
                 n -= 1
